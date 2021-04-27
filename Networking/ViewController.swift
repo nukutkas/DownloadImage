36d329c1558f3976f3f5782ce064f072e68ca223
Nikita Kochetov <nothsmple@mail.ru>
2021-04-27 23:08:08 +0300

    Activity Indicator has been added

--- Networking/ViewController.swift
+++ Networking/ViewController.swift
@@ -9,19 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var label: UILabel!
-    @IBOutlet weak var getImageButton: UIButton!
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var label: UILabel!
+    @IBOutlet var getImageButton: UIButton!
+    @IBOutlet var activityIndicator: UIActivityIndicatorView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        activityIndicator.isHidden = true
+        activityIndicator.hidesWhenStopped = true
+    }
     
     @IBAction func getImagePressed(_ sender: Any) {
+        
+        label.isHidden = true
+        getImageButton.isEnabled = false
+        activityIndicator.isHidden = false
+        activityIndicator.startAnimating()
+        
+        
         guard let url = URL(string: "https://applelives.com/wp-content/uploads/2016/03/iPhone-SE-11.jpeg") else { return }
         let session = URLSession.shared
         
         session.dataTask(with: url) { (data, response, error) in
             if let data = data, let image = UIImage(data: data) {
                 DispatchQueue.main.async {
-                    self.label.isHidden = true
-                    self.getImageButton.isEnabled = false
+                    self.activityIndicator.stopAnimating()
                     self.imageView.image = image
                 }
             }
