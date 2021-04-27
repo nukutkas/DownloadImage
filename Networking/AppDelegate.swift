e813c5f226083acbb82309973d13cdefc5b581ae
Nikita Kochetov <nothsmple@mail.ru>
2021-04-27 22:18:59 +0300

    Initial Commit

---
+++ Networking/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Networking
+//
+//  Created by Татьяна Кочетова on 21.04.2021.
+
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ Networking/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Networking
+//
+//  Created by Татьяна Кочетова on 21.04.2021.
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var getImageButton: UIButton!
+
+    @IBAction func getImagePressed(_ sender: Any) {
+    }
+    
+}
+
