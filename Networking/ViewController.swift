03c60f8026f67f313be80a4c6ed55d9072d9fd5a
Nikita Kochetov <nothsmple@mail.ru>
2021-04-27 22:45:44 +0300

    getImagePressed realized

--- Networking/ViewController.swift
+++ Networking/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var label: UILabel!
     @IBOutlet weak var getImageButton: UIButton!
-
+    
     @IBAction func getImagePressed(_ sender: Any) {
+        guard let url = URL(string: "https://applelives.com/wp-content/uploads/2016/03/iPhone-SE-11.jpeg") else { return }
+        let session = URLSession.shared
+        
+        session.dataTask(with: url) { (data, response, error) in
+            if let data = data, let image = UIImage(data: data) {
+                DispatchQueue.main.async {
+                    self.label.isHidden = true
+                    self.getImageButton.isEnabled = false
+                    self.imageView.image = image
+                }
+            }
+        }.resume()
     }
     
 }
